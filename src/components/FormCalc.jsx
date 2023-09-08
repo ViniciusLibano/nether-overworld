d468ef9045ce8778dc9177d3ebbab498d8c353db
Vinicius Libano <34941776+ViniciusLibano@users.noreply.github.com>
2023-09-08 11:53:56 -0300

    coords convert update

--- src/components/FormCalc.jsx
+++ src/components/FormCalc.jsx
@@ -1,11 +1,12 @@
 import styles from './FormCalc.module.scss'
 
+// eslint-disable-next-line react/prop-types
 export default function FormCalc({calcName, xValue, yValue, zValue, setXValue, setYValue, setZValue}) {
     
     let x = 'x' + String(calcName).toLowerCase()
     let y = 'y' + String(calcName).toLowerCase()
     let z = 'z' + String(calcName).toLowerCase()
-    
+
     return (
         <div className={styles.FormCalc}>
             <h3>{String(calcName).toUpperCase()}</h3>
--- src/components/MainInt.jsx
+++ src/components/MainInt.jsx
@@ -1,4 +1,3 @@
-import FormCalc from './FormCalc'
 import styles from './MainInt.module.scss'
 import { useState } from 'react'
 
@@ -9,6 +8,41 @@ export default function MainInt() {
     const [yNether, setYNether] = useState()
     const [zOwrd, setZOwrd] = useState()
     const [zNether, setZNether] = useState()
+
+
+    //definindo o X
+    const handleInputOwrdX = (e) => {
+        setXOwrd(e.target.value)
+        setXNether(toNether(e.target.value))
+    }
+
+    const handleInputNetherX = (e) => {
+        setXNether(e.target.value)
+        setXOwrd(toOverworld(e.target.value))
+    }
+
+    //definindo o Y
+    const handleInputOwrdY = (e) => {
+        setYOwrd(e.target.value)
+        setYNether(toNether(e.target.value))
+    }
+
+    const handleInputNetherY = (e) => {
+        setYNether(e.target.value)
+        setYOwrd(toOverworld(e.target.value))
+    }
+
+    //definindo o Z
+    const handleInputOwrdZ = (e) => {
+        setZOwrd(e.target.value)
+        setZNether(toNether(e.target.value))
+    }
+
+    const handleInputNetherZ = (e) => {
+        setZNether(e.target.value)
+        setZOwrd(toOverworld(e.target.value))
+    }
+
 
     const toOverworld = (n) => {
         if (isNaN(n)) {
@@ -23,13 +57,44 @@ export default function MainInt() {
             n = 0
         }
 
-        return Math.trunc(0/8)
+        return Math.trunc(n/8)
     }
 
     return (
         <div className={styles.MainInt}>
-            <FormCalc calcName='overworld' xValue={toOverworld(xNether)} yValue={toOverworld(yNether)} zValue={toOverworld(zNether)}/>
-            <FormCalc calcName='nether' setXValue={setXNether} setYValue={setYNether} setZValue={setZNether}/>
+            <div className={styles.FormCalc}>
+                <h3>Overworld</h3>
+                <p>Coord. Aproximadas</p>
+                <div>
+                    <label htmlFor="xoverworld">X</label>
+                    <input name="xoverworld" type="text" onChange={handleInputOwrdX} value={xOwrd} />
+                </div>
+                <div>
+                    <label htmlFor="yoverworld">Y</label>
+                    <input name="yoverworld" type="text" onChange={handleInputOwrdY} value={yOwrd} />
+                </div>
+                <div>
+                    <label htmlFor="zoverworld">Z</label>
+                    <input name="zoverworld" type="text" onChange={handleInputOwrdZ} value={zOwrd} />
+                </div>
+            </div>
+
+            <div className={styles.FormCalc}>
+                <h3>Nether</h3>
+                <p>Coord. Aproximadas</p>
+                <div>
+                    <label htmlFor="xnether">X</label>
+                    <input name="xnether" type="text" onChange={handleInputNetherX} value={xNether} />
+                </div>
+                <div>
+                    <label htmlFor="ynether">Y</label>
+                    <input name="ynether" type="text" onChange={handleInputNetherY} value={yNether} />
+                </div>
+                <div>
+                    <label htmlFor="znether">Z</label>
+                    <input name="znether" type="text" onChange={handleInputNetherZ} value={zNether} />
+                </div>
+            </div>
         </div>
     )
 }
